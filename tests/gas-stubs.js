30f27a8dd90d5f7389280c013a5805262a654efd
Chet Insolia <cinsolia@gmail.com>
2018-10-21 17:03:33 -0400

    1st attempt at manual gas object fake (unfinished, commented out)

---
+++ tests/gas-stubs.js
@@ -0,0 +1,24 @@
+// function calendarGuestStub(){
+//     return {
+//         getEmail: () => {} 
+//     }
+// }
+
+// function calendarEventStub(options){
+//     options = options || {};
+//     //const date = ;
+    
+//     return {
+//         getId: () => {options.id || ""}, 
+//         getTitle: () => {options.title || ""}, 
+//         getDescription: () => {options.description || ""}, 
+//         getLocation: () => {options.locations || ""}, 
+//         getGuestList: () => {options.guestList || ""}, 
+//         getColor: () => {options.color || ""}, 
+//         isAllDayEvent: () => {options.isAllDayEvent || false}, 
+//         getAllDayStartDate: () => {options.allDayStartDate || date}, 
+//         getAllDayEndDate: () => {options.allDayEndDate || date}, 
+//         getStartTime: () => {options.getStartTime || date}, 
+//         getEndTime: () => {options.getEndTime || date}, 
+//     }
+// }
